f720db7a77e61ebd3dc68367839e183eaf468669
Van Boughner <van.boughner@ntti3.io>
2018-03-23 13:06:16 -0700

    removed body from identity-validate response

--- src/lambda/identity-validate.js
+++ src/lambda/identity-validate.js
@@ -3,7 +3,6 @@ exports.handler = function(event, context, callback) {
     console.log(event);
     console.log(context);
     callback(null, {
-        statusCode: 200,
-        body: "identity validate"
+        statusCode: 200
     });
 };

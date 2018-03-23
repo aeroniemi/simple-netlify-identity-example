ea56e206039d2c4eedb54bd3e2ba39e0622dcab4
Van Boughner <van.boughner@ntti3.io>
2018-03-23 11:23:33 -0700

    updated login script to use a correct payload (according to docs)

--- src/lambda/identity-login-disabled.js
+++
@@ -1,9 +0,0 @@
-exports.handler = function(event, context, callback) {
-    console.log("identity login");
-    console.log(event);
-    console.log(context);
-	callback(null, {
-        statusCode: 200,
-        body: "identity login"
-    });
-};

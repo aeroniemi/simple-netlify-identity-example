b3cf44d80061c79f6f0d0a493c836639ca3f99ee
Van Boughner <van.boughner@ntti3.io>
2018-03-28 11:07:00 -0700

    cleaned up code in webhook now that put is working somewhat

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -29,63 +29,36 @@ let putAdminRoleOnUser = function(identityUrl, userId, token) {
 };
 
 exports.handler = function (event, context, callback) {
+    let body = JSON.parse(event.body);
     let retval = 200;
+
     console.log("webhook");
     console.log("event", event);
     console.log("context", context);
-    hipchat.notify({
-        message: 'webhook, context.clientContext: ' + JSON.stringify(context.clientContext) +
-        ' the token is ' + context.clientContext.identity.token + ' and the url is ' +
-        context.clientContext.identity.url,
-        color: 'green'
-    });
-    let body = JSON.parse(event.body);
-    hipchat.notify({
-        message: 'webhook, body.event: ' + JSON.stringify(body.event),
-        color: 'yellow'
-    });
-    hipchat.notify({
-        message: 'webhook, body.user: ' + JSON.stringify(body.user),
-        color: 'yellow'
-    });
 
     if (body.event === 'validate') {
-        // only allow signups from ntti3.io domain
-        let allow = false;
-        if (body.user.email) {
-            hipchat.notify({
-                message: 'EXAMINE: email of new user ' + body.user.email,
-                color: 'red'
-            });
-
-            if (body.user.email.match(/@ntti3.io$/i)) {
-                allow = true;
-            }
-        }
-
+        // allow new signups only from ntti3.io domain
+        let allow = (body.user.email && body.user.email.match(/@ntti3.io$/i));
         retval = allow ? 200 : 401;
     }
     else if (body.event === 'signup') {
-        // figure out how to give the 'admin' role to the newly signed up user
-        hipchat.notify({
-            message: 'give admin role to new user ' + body.user.email,
-            color: 'red'
-        });
-
-        putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+        // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
+        if (!body.user.app_metadata.roles) {
+            putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+        }
     }
     else if (body.event === 'login') {
-        hipchat.notify({
-            message: 'TODO: try again to give admin role to user ' + body.user.email,
-            color: 'red'
-        });
-
-        putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+        // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
+        // TODO: figure out why this is necessary, it should have been handled during signup
+        if (!body.user.app_metadata.roles) {
+            putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+        }
     }
 
     hipchat.notify({
-        message: 'webhook, returning code : ' + retval,
-        color: 'red'
+        message: 'webhook, returning code: ' + retval + ' while doing ' +
+            JSON.stringify(body.event) + ' for user: ' + JSON.stringify(body.user),
+        color: 'yellow'
     });
 
     callback(null, {

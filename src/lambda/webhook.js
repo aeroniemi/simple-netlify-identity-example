7e74200c7501dc282bd465c7f01956b88b74b98f
Van Boughner <van.boughner@ntti3.io>
2018-03-28 07:40:45 -0700

    modified webhook experiment script

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -2,34 +2,37 @@
 const HipChatNotify = require('hipchat-notify');
 let hipchat = new HipChatNotify(4516663, 'tn8neyu93ei7x3jRnYrHuzNtq2RbXXGuIXwZTmwn');
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     let retval = 200;
-    const user = context.clientContext.user;
     console.log("webhook");
-    console.log(event);
-    console.log(context);
+    console.log("event", event);
+    console.log("context", context);
     hipchat.notify({
         message: 'webhook, context.clientContext.identity: ' + JSON.stringify(context.clientContext.identity),
         color: 'green'
     });
     hipchat.notify({
-        message: 'webhook, event.body.event: ' + JSON.stringify(event.body.event),
+        message: 'webhook, context.clientContext.user: ' + JSON.stringify(context.clientContext.user),
         color: 'green'
+    });
+    hipchat.notify({
+        message: 'webhook, context: ' + JSON.stringify(context),
+        color: 'green'
+    });
+    hipchat.notify({
+        message: 'webhook, event.body.event: ' + JSON.stringify(event.body.event),
+        color: 'yellow'
     });
     hipchat.notify({
         message: 'webhook, event.body.user: ' + JSON.stringify(event.body.user),
-        color: 'green'
+        color: 'yello'
     });
     hipchat.notify({
-            message: 'webhook, event: ' + JSON.stringify(event),
-        color: 'green'
+        message: 'webhook, event: ' + JSON.stringify(event),
+        color: 'yellow'
     });
     hipchat.notify({
-            message: 'webhook, context: ' + JSON.stringify(context),
-        color: 'green'
-    });
-    hipchat.notify({
-            message: 'webhook, returning code : ' + retval,
+        message: 'webhook, returning code : ' + retval,
         color: 'gray'
     });
 

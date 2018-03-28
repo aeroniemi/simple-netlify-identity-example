b95ab3b7c39096b71a25e0e174764ee77563d316
Van Boughner <van.boughner@ntti3.io>
2018-03-28 13:21:16 -0700

    removed experiment with returning new role info in validate body

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -31,7 +31,6 @@ let putAdminRoleOnUser = function(identityUrl, userId, token) {
 exports.handler = function (event, context, callback) {
     let body = JSON.parse(event.body);
     let retval = 200;
-    let outputBody = null;
 
     console.log("webhook");
     console.log("event", event);
@@ -40,11 +39,6 @@ exports.handler = function (event, context, callback) {
     if (body.event === 'validate') {
         // allow new signups only from ntti3.io domain
         let allow = (body.user.email && body.user.email.match(/@ntti3.io$/i));
-        if (allow) {
-            outputBody = {
-                "app_metadata": {"roles": ["admin"]}
-            };
-        }
         retval = allow ? 200 : 401;
     }
     else if (body.event === 'signup') {
@@ -67,15 +61,7 @@ exports.handler = function (event, context, callback) {
         color: 'yellow'
     });
 
-    if (outputBody) {
-        callback(null, {
-            statusCode: retval,
-            body: outputBody
-        });
-    }
-    else {
-        callback(null, {
-            statusCode: retval
-        });
-    }
+    callback(null, {
+        statusCode: retval
+    });
 };

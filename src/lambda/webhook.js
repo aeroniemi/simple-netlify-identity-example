078c0f24ac56105c1d986371d7d5295c3d673ba4
Van Boughner <van.boughner@ntti3.io>
2018-03-28 14:11:46 -0700

    removed experiments in user creation during validate, it created multiple copies of the user

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -28,31 +28,6 @@ let putAdminRoleOnUser = function(identityUrl, userId, token) {
     });
 };
 
-let postCreateUserWithAdminRole = function(identityUrl, user, token) {
-    // using this library for http requests: https://github.com/request/request
-    // using this library at the api endpoint: https://github.com/netlify/gotrue
-    // use the POST /admin/users api endpoint to create a new user record with role
-    if (user && user.app_metadata) {
-        user.app_metadata.roles = ["admin"];
-    }
-    let postOptions = {
-        url: identityUrl + '/admin/users',
-        method: 'POST',
-        headers: {
-            Authorization: 'Bearer ' + token
-        },
-        json: true,
-        body: user
-    };
-    request(postOptions, function(error, response, body) {
-        hipchat.notify({
-            message: 'webhook, POST request with these options: ' + JSON.stringify(postOptions) +
-            ' and this response ' + JSON.stringify(response) + ' and this body ' + JSON.stringify(body),
-            color: 'red'
-        });
-    });
-};
-
 exports.handler = function (event, context, callback) {
     let body = JSON.parse(event.body);
     let retval = 200;
@@ -65,13 +40,6 @@ exports.handler = function (event, context, callback) {
         // allow new signups only from ntti3.io domain
         let allow = (body.user.email && body.user.email.match(/@ntti3.io$/i));
         retval = allow ? 200 : 401;
-        if (allow) {
-            hipchat.notify({
-                message: 'webhook, validate has user: ' + JSON.stringify(body.user),
-                color: 'yellow'
-            });
-            postCreateUserWithAdminRole(context.clientContext.identity.url, body.user, context.clientContext.identity.token);
-        }
     }
     else if (body.event === 'signup') {
         // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)

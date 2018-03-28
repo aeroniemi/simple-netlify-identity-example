38a6e2df33c6cde4b56adcc1ab6b359d18a13823
Van Boughner <van.boughner@ntti3.io>
2018-03-28 14:20:29 -0700

    changed the name of the main role to reader

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -2,12 +2,13 @@
 const request = require('request');
 const HipChatNotify = require('hipchat-notify');
 let hipchat = new HipChatNotify(4516663, 'tn8neyu93ei7x3jRnYrHuzNtq2RbXXGuIXwZTmwn');
+let readerRoleName = 'reader';
 
-let putAdminRoleOnUser = function(identityUrl, userId, token) {
+let putReaderRoleOnUser = function(identityUrl, userId, token) {
     // using this library for http requests: https://github.com/request/request
     // using this library at the api endpoint: https://github.com/netlify/gotrue
     // use the PUT /admin/users api endpoint to add this to user record:
-    // {"app_metadata": {"authorization": {"roles": ["admin"]}}}
+    // {"app_metadata": {"roles": ["reader"]}}}
     let putOptions = {
         url: identityUrl + '/admin/users/' + userId,
         method: 'PUT',
@@ -16,13 +17,14 @@ let putAdminRoleOnUser = function(identityUrl, userId, token) {
         },
         json: true,
         body: {
-            app_metadata: {"roles": ["admin"]}
+            app_metadata: {'roles': [readerRoleName]}
         }
     };
     request(putOptions, function(error, response, body) {
         hipchat.notify({
-            message: 'webhook, PUT request for userId=' + userId + ' error=' + error +
-            ' response: ' + JSON.stringify(response) + ' ==== body: ' + JSON.stringify(body),
+            message: 'put options are === ' + JSON.stringify(putOptions) +
+            ' === and response is === ' + JSON.stringify(response) +
+            ' === and body is === ' + JSON.stringify(body),
             color: 'purple'
         });
     });
@@ -44,19 +46,19 @@ exports.handler = function (event, context, callback) {
     else if (body.event === 'signup') {
         // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
         if (!body.user.app_metadata.roles) {
-            putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+            putReaderRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
         }
     }
     else if (body.event === 'login') {
         // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
         // TODO: figure out why this is necessary, it should have been handled during signup
         if (!body.user.app_metadata.roles) {
-            putAdminRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+            putReaderRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
         }
     }
 
     hipchat.notify({
-        message: 'webhook, returning code: ' + retval + ' while doing ' +
+        message: 'webhook returning code: ' + retval + ' while doing ' +
             JSON.stringify(body.event) + ' for user: ' + JSON.stringify(body.user),
         color: 'yellow'
     });

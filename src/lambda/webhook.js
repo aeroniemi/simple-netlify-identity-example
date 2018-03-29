9dcdd9b0a0a35b7981c2efe2a99ad5f70acc6527
van <van@hovercloud.com>
2018-03-29 10:38:12 -0700

    cleaned up the code in the webhook

--- src/lambda/webhook.js
+++ src/lambda/webhook.js
@@ -1,81 +1,52 @@
-
-const request = require('request');
-const HipChatNotify = require('hipchat-notify');
-let hipchat = new HipChatNotify(4516663, 'tn8neyu93ei7x3jRnYrHuzNtq2RbXXGuIXwZTmwn');
-let readerRoleName = 'reader';
-
-let putReaderRoleOnUser = function(identityUrl, userId, token) {
-    // using this library for http requests: https://github.com/request/request
-    // using this library at the api endpoint: https://github.com/netlify/gotrue
-    // use the PUT /admin/users api endpoint to add this to user record:
-    // {"app_metadata": {"roles": ["reader"]}}}
-    let putOptions = {
-        url: identityUrl + '/admin/users/' + userId,
-        method: 'PUT',
-        headers: {
-            Authorization: 'Bearer ' + token
-        },
-        json: true,
-        body: {
-            app_metadata: {'roles': [readerRoleName]}
-        }
-    };
-    request(putOptions, function(error, response, body) {
-        hipchat.notify({
-            message: 'put options are === ' + JSON.stringify(putOptions) +
-            ' === and response is === ' + JSON.stringify(response) +
-            ' === and body is === ' + JSON.stringify(body),
-            color: 'purple'
-        });
-    });
-};
+/*
+ * This webhook is called whenever a new user tries to sign up on the site using the Netlify Identity Wisget.
+ * It checks that the user's email is on a permitted domain (such as username@ntti3.io),
+ * and will only allow new users to sign up from permitted domains. Upon a valid signup, the new user is
+ * automatically given the 'reader' role, so that they may view everything on the website.
+ *
+ * To get this script called by Netlify, you need to activate the webhook, here are the steps:
+ *     - go to the Netlify Site Settings for your web site
+ *     - look for the Settings -> Identity -> Notifications section
+ *     - edit the Webhook
+ *     - set the URL to https://<your-site-name>.netlify.com/.netlify/functions/webhook
+ *     - leave the Secret field blank
+ *     - check the event box for validate
+ *     - click on the Save button
+ *
+ * In addition, the site should have a _redirects file that limits access to those with particular roles.
+ * This will restrict visitor access to only those people with a role that permits access.
+ *
+ * Here's what the _redirects file should look like for your static website when you publish it on Netlify:
+ *
+ *     /test/* 200! Role=reader
+ *     /test/ /login 404
+ *
+ * (replace test with the name of your protected sub-folder, and login with the name of the fail html file)
+ *
+ * More information about Netlify Identity service and Functions here:
+ *     - https://www.netlify.com/docs/identity/
+ *     - https://www.netlify.com/docs/functions/#identity-and-functions
+ */
 
 exports.handler = function (event, context, callback) {
     let body = JSON.parse(event.body);
-    let retval = 200;
-    let responseBody = null;
-
-    console.log("webhook");
-    console.log("event", event);
-    console.log("context", context);
-
     if (body.event === 'validate') {
-        // allow new signups only from ntti3.io domain
-        let allow = (body.user.email && body.user.email.match(/@ntti3.io$/i));
-        retval = allow ? 200 : 401;
-        if (allow) {
-            responseBody = {"app_metadata": {"roles": [readerRoleName]}};
+        if (body.user.email && body.user.email.match(/@ntti3.io$/i)) {
+            callback(null, {
+                statusCode: 200,
+                body: {"app_metadata": {"roles": ["reader"]}}
+            });
         }
-    }
-    else if (body.event === 'signup') {
-        // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
-        if (!body.user.app_metadata.roles) {
-            putReaderRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
+        else {
+            callback(null, {
+                statusCode: 401
+            });
         }
-    }
-    else if (body.event === 'login') {
-        // if this user doesn't have any roles yet, then add admin role (typically for newly signed up user)
-        // TODO: figure out why this is necessary, it should have been handled during signup
-        if (!body.user.app_metadata.roles) {
-            putReaderRoleOnUser(context.clientContext.identity.url, body.user.id, context.clientContext.identity.token);
-        }
-    }
-
-    hipchat.notify({
-        message: 'webhook returning code: ' + retval + ' with responseBody=' + JSON.stringify(responseBody) +
-                 ' while doing ' + JSON.stringify(body.event) + ' for user: ' + JSON.stringify(body.user),
-        color: 'yellow'
-    });
-
-    if (responseBody) {
-        callback(null, {
-            statusCode: retval,
-            body: JSON.stringify(responseBody)
-        });
     }
     else {
+        // if you happen to enable this webhook for signup or login events, this will make sure they still succeed
         callback(null, {
-            statusCode: retval
+            statusCode: 200
         });
     }
 };
